a215672e34c472c8678a79facd81c7ebe2916b95
bradleylbs <swelihlelucas@gmail.com>
2024-11-06 13:15:06 +0200

    nxt updte

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig: NextConfig = {output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  basePath: '/digital-transport-dashboard', // GitHub repo name
+  assetPrefix: '/digital-transport-dashboard', // GitHub repo name
 };
 
 export default nextConfig;
